60bda6c3df16d785f75ff44042fc0e360848fa5b
DongjuLee <dozoo0528@gmail.com>
2026-03-19 14:38:02 +0900

    week03 v0.4

--- week02.py
+++ week02.py
@@ -1,5 +1,5 @@
 # v0.3
-import statistics
+import statistics #통계모듈 (각종 통계함수를 제공), 파이썬으로 제작된 내장 모듈
 scores = [100, 87, 97, 82]
 average = statistics.mean(scores)
 print(average)
---
+++ week03.py
@@ -0,0 +1,6 @@
+# v0.1
+import numpy # 써드파티 라이브러리, C계열 기반으로 대규모 데이터처리에 적합. 매우 빠름
+
+scores = [100, 97, 88, 91]
+average = numpy.mean(scores)
+print(average)
