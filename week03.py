582f4fcb3e527fb5a3296fbd958cf9538cd27304
DongjuLee <dozoo0528@gmail.com>
2026-03-19 14:50:15 +0900

    week03 v0.5

--- week03.py
+++ week03.py
@@ -1,6 +1,7 @@
 # v0.1
-import numpy # 써드파티 라이브러리, C계열 기반으로 대규모 데이터처리에 적합. 매우 빠름
+import numpy as np # 써드파티 라이브러리, C계열 기반으로 대규모 데이터처리에 적합. 매우 빠름
+#
 
 scores = [100, 97, 88, 91]
-average = numpy.mean(scores)
+average = np.mean(scores)
 print(average)
