ea9d25f8054555e50379783d754edb5269e6cf8d
DongjuLee <dozoo0528@gmail.com>
2026-04-30 15:37:59 +0900

    week09 pandas

--- week09.py
+++ week09.py
@@ -1,19 +1,14 @@
-from os import rename
-
-import numpy as np
 import pandas as pd
 
-df = pd.DataFrame(data={'국':[1, 2, 3], '영':[6, 4, 5], '수':[7, 8, 9]}, index=[1, 2, 3])
-print(df)
-
-df_new = (
-    pd.melt(df)
-    .rename(columns={
-        'variable': 'var',
-        'value': 'val'
-    })
-    .query('val >= 5')
-    .sort_values('val', ascending=False)
+df = pd.DataFrame(
+    data={
+        '국': [1, 6, 7],
+        '영': [2, 4, 8],
+        '수': [3, 5, 9],
+        '미세먼지': [10, 3, 11]
+    },
+    index=[1, 2, 3]
 )
-
+print(df)
+df_new = df.drop(columns=['수', '미세먼지'])
 print(df_new)
