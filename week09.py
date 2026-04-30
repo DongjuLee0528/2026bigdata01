19c7efa7377595d82bbee0cf4fabef909bc22d2a
DongjuLee <dozoo0528@gmail.com>
2026-04-30 14:49:30 +0900

    week09 pandas

--- week09.py
+++ week09.py
@@ -1,18 +1,18 @@
+from os import rename
+
 import numpy as np
 import pandas as pd
 
-aee2d = np.array([
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-    ]
+df = pd.DataFrame(data={'국':[1, 2, 3], '영':[6, 4, 5], '수':[7, 8, 9]}, index=[1, 2, 3])
+print(df)
+
+df_new = (
+    pd.melt(df)
+    .rename(columns={
+        'variable': 'var',
+        'value': 'val'
+    })
+    .query('val >= 5')
 )
-# df_2dlist = pd.DataFrame(arr2d, columns=['국', '영', '수'], index=[1, 2, 3])
 
-# Creating DataFr
-
-df_2dict = pd.DataFrame(aee2d, columns=['국', '영','수'], index=[1, 2, 3])
-df_dict = pd.DataFrame(data={'국':[1, 2, 3], '영':[6, 4, 5], '수':[7, 8, 9]}, index=[1, 2, 3])
-print(aee2d)
-print(df_2dict)
-print(df_dict)
+print(df_new)
