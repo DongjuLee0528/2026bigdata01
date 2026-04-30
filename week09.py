9dd3e9cb9c890abaa054328280285d9eb7690229
DongjuLee <dozoo0528@gmail.com>
2026-04-30 15:48:47 +0900

    week09 pandas

--- week09.py
+++ week09.py
@@ -5,10 +5,12 @@ df = pd.DataFrame(
         '국': [1, 6, 7],
         '영': [2, 4, 8],
         '수': [3, 5, 9],
-        '미세먼지': [10, 3, 11]
+        '화': [10, 3, 11]
     },
     index=[1, 2, 3]
 )
 print(df)
-df_new = df.drop(columns=['수', '미세먼지'])
+#df_new = df.iloc[0:3] =df_new = df.iloc[:]
+#df_new = df.iloc[1:3]
+df_new = df.iloc[1:3,2:4]
 print(df_new)
