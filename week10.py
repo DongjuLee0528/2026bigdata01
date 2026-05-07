0095e55f34b1101d1ce1c3c34fc51b17f678b7ec
DongjuLee <dozoo0528@gmail.com>
2026-05-07 15:08:07 +0900

    week10

--- week10.py
+++ week10.py
@@ -2,11 +2,16 @@ import pandas as pd
 
 df = pd.DataFrame(
     data={
-        '국': ['1', '6', '7'],
-        '영': ['2', '4', '8'],
-        '수': ['3',' 5',' 9'],
-        '화': ['10', '3', '11']
+        '국': [1, 6, 7],
+        '영': [2, 4, 8],
+        '수': [3, 5, 9],
+        '화': [10, 3, 11]
     },
     index=[1, 2, 3]
 )
 print(df)
+#print(df.describe())
+# print(df.info())
+# print(df['국'].value_counts() )
+# print(df.shape)
+print(df['국'].nunique())
